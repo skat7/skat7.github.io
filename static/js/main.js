cecb6f59edee1d67373f792aa177d0d68a486807
skat7 <84645513+skat7@users.noreply.github.com>
2021-07-24 12:46:37 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -185,3 +185,41 @@ setTimeout(function(){
 
 
 }, 3500);
+
+
+
+
+let scrollerID;
+let paused = true;
+let speed = 2; // 1 - Fast | 2 - Medium | 3 - Slow
+let interval = speed * 5;
+
+function startScroll(){
+    let id = setInterval(function() {
+        window.scrollBy(0, 2);
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            // Reached end of page
+            stopScroll();
+        }
+    }, interval);
+    return id;
+}
+
+function stopScroll() {
+    clearInterval(scrollerID);
+}
+
+document.body.addEventListener('keypress', function (event)
+{
+    if (event.which == 19 || event.keyCode == 19) {
+        // It's the 'Enter' key
+        if(paused == true) {
+            scrollerID = startScroll();
+            paused = false;
+        }
+        else {
+            stopScroll();
+            paused = true;
+        }
+    }
+}, true);
